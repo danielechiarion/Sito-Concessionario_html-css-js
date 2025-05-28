420113c4d0fc414c9f3a3c858ec3842a34255f16
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2025-05-28 10:58:23 +0200

    continue admin add-items js code

--- server_localhost/SimpleStaticServer.java
+++ server_localhost/SimpleStaticServer.java
@@ -44,61 +44,80 @@ public class SimpleStaticServer {
         public void handle(HttpExchange var1) throws IOException {
             String var2 = var1.getRequestURI().getPath();
             File var3 = (new File(this.rootDir, var2)).getCanonicalFile();
-
+        
             if (!var3.getPath().startsWith((new File(this.rootDir)).getCanonicalPath())) {
                 this.send404(var1);
             } else {
                 if (var3.isDirectory()) {
                     var3 = new File(var3, "index.html");
                 }
-
+        
                 if (var3.exists() && var3.isFile()) {
                     String var4 = Files.probeContentType(var3.toPath());
+                    
+                    // Fallback per i tipi MIME
                     if (var4 == null) {
-                    var4 = "application/octet-stream";
+                        String fileName = var3.getName();
+                        if (fileName.endsWith(".html")) {
+                            var4 = "text/html";
+                        } else if (fileName.endsWith(".css")) {
+                            var4 = "text/css";
+                        } else if (fileName.endsWith(".js")) {
+                            var4 = "application/javascript";
+                        } else if (fileName.endsWith(".png")) {
+                            var4 = "image/png";
+                        } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+                            var4 = "image/jpeg";
+                        } else if (fileName.endsWith(".gif")) {
+                            var4 = "image/gif";
+                        } else if (fileName.endsWith(".svg")) {
+                            var4 = "image/svg+xml";
+                        } else {
+                            var4 = "application/octet-stream"; // Default MIME type
+                        }
                     }
-
+        
                     var1.getResponseHeaders().add("Content-Type", var4);
                     var1.sendResponseHeaders(200, var3.length());
                     OutputStream var5 = var1.getResponseBody();
-
+        
                     try {
-                    FileInputStream var6 = new FileInputStream(var3);
-
-                    try {
-                        byte[] var7 = new byte[8192];
-
-                        int var8;
-                        while((var8 = var6.read(var7)) != -1) {
-                            var5.write(var7, 0, var8);
-                        }
-                    } catch (Throwable var11) {
+                        FileInputStream var6 = new FileInputStream(var3);
+        
                         try {
-                            var6.close();
-                        } catch (Throwable var10) {
-                            var11.addSuppressed(var10);
+                            byte[] var7 = new byte[8192];
+        
+                            int var8;
+                            while ((var8 = var6.read(var7)) != -1) {
+                                var5.write(var7, 0, var8);
+                            }
+                        } catch (Throwable var11) {
+                            try {
+                                var6.close();
+                            } catch (Throwable var10) {
+                                var11.addSuppressed(var10);
+                            }
+        
+                            throw var11;
                         }
-
-                        throw var11;
-                    }
-
-                    var6.close();
+        
+                        var6.close();
                     } catch (Throwable var12) {
-                    if (var5 != null) {
-                        try {
-                            var5.close();
-                        } catch (Throwable var9) {
-                            var12.addSuppressed(var9);
+                        if (var5 != null) {
+                            try {
+                                var5.close();
+                            } catch (Throwable var9) {
+                                var12.addSuppressed(var9);
+                            }
                         }
+        
+                        throw var12;
                     }
-
-                    throw var12;
-                    }
-
+        
                     if (var5 != null) {
-                    var5.close();
+                        var5.close();
                     }
-
+        
                 } else {
                     this.send404(var1);
                 }
